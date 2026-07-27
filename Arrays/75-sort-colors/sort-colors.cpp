e2fc5497a6b4edb460853e29564405e712c37f52
Nitish Harbola <143637458+Fury-Senpai@users.noreply.github.com>
2026-07-27 13:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.37%) - LeetSync

---
+++ Arrays/75-sort-colors/sort-colors.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        // counting sort way
+        
+        vector<int> count = {0,0,0};
+
+        for(int i = 0; i<nums.size(); i++) {
+            if(nums[i] == 0) {
+                count[0]++;
+            } else if(nums[i] == 1) {
+                count[1]++;
+            } else  {
+                count[2]++;
+            }
+        } 
+
+        nums.clear();
+
+        for(int i = 0; i<count.size(); i++) {
+            for(int j = 0; j<count[i]; j++) {
+                
+                nums.push_back(i);
+            }
+        }
+       
+    }
+};
